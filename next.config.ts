72351f6f89debc339f5da33b48bb854380614801
ishaan <ishaan.mohan.sinha@gmail.com>
2024-10-28 14:54:27 -0400

    website fully published

--- next.config.ts
+++ next.config.ts
@@ -4,13 +4,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export",
   reactStrictMode: true,
   images: {
     unoptimized: true,
-  },
-  assetPrefix: '/imsinha0.github.io/',
-  basePath: '/imsinha0.github.io',
-  output: 'export'
+  }
 };
 
 export default nextConfig;
